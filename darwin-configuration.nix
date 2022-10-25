a6c4a13c1546db31830ddc6fbfb55477a2a90ef1
Robin Bozan <contact@robinbozan.nl>
2022-10-25 17:48:42 +0200

    Update darwin-configuration.nix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,47 +1,55 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    with pkgs; [
-# firefox
-helix
-git
-neovim
+	# List packages installed in system profile. To search by name, run:
+	# $ nix-env -qaP | grep wget
+	environment.systemPackages = with pkgs; [
+		# firefox
+		helix
+		git
+		neovim
 
-vscode
-slack
-# google-chrome
-    ];
+		nodejs
 
- nixpkgs.config.allowUnfree = true; # for vscode
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+		vscode
+		slack
+		# google-chrome
+	];
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+	nixpkgs.config.allowUnfree = true; # for vscode
+	# Use a custom configuration.nix location.
+	# $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+	# environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
+	# Auto upgrade nix package and the daemon service.
+	services.nix-daemon.enable = true;
+	# nix.package = pkgs.nix;
 
-users.users = {
-rbozan = {
-description = "Robin Bozan";
-createHome = true;
-isHidden = false;
-};
-};
+	# Create /etc/zshrc that loads the nix-darwin environment.
+	programs.zsh.enable = true;  # default shell on catalina
+	# programs.fish.enable = true;
+
+	homebrew = {
+		enable = true;
+		casks = [ "google-chrome" "firefox" ];
+	};
+
+	users.users = {
+		rbozan = {
+			description = "Robin Bozan";
+			createHome = true;
+			isHidden = false;
+		};
+	};
+
 
 	system.defaults = {
 		loginwindow.GuestEnabled = true;
-		NSGlobalDomain.com.apple.swipescrolldirection = false;
-	}
+		dock.orientation = "left";
+#		NSGlobalDomain.com.apple.swipescrolldirection = false;
+	};
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+	# Used for backwards compatibility, please read the changelog before changing.
+	# $ darwin-rebuild changelog
+	system.stateVersion = 4;
 }
